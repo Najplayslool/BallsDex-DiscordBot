72c210cf9073009273c911542a7ac2e0e66a9f59
Najplayslool <109397117+Najplayslool@users.noreply.github.com>
2025-04-13 10:39:35 +0400

    Upload ballsdex.packagex.boxes in config
    Updated /daily command:
    
    - Ball Dex card pops up when packed
    - Shows Rarity, Ball Name, Ball Emoji
    - added user avatars in pack command incase two people open it at the same time.

--- ballsdex/packages/boxes/cog.py
+++ ballsdex/packages/boxes/cog.py
@@ -38,8 +38,12 @@ last_daily_times = {}
 # Cooldowns
 DAILY_COOLDOWN = timedelta(hours=24)
 
-class Claim(commands.GroupCog):
-    def __init__(self, bot):
+class Claim(commands.GroupCog, group_name="players"):
+    """
+    a little simple daily pack!
+    """
+
+    def __init__(self, bot: "BallsDexBot"):
         self.bot = bot
 
     async def get_random_ball(self):
@@ -59,7 +63,7 @@ class Claim(commands.GroupCog):
             if datetime.now() - last < DAILY_COOLDOWN:
                 remaining = DAILY_COOLDOWN - (datetime.now() - last)
                 await interaction.response.send_message(
-                    f"You must wait {remaining} before claiming your next daily reward.",
+                    f"You must wait {remaining} before claiming your next daily reward! • Made by drift",
                     ephemeral=True
                 )
                 return
@@ -78,16 +82,35 @@ class Claim(commands.GroupCog):
         )
 
         emoji = self.bot.get_emoji(ball.emoji_id)
+
+        color_choice = random.choice([
+            discord.Color.from_rgb(229, 255, 0),
+            discord.Color.from_rgb(255, 0, 0),
+            discord.Color.from_rgb(0, 17, 255)
+        ])
         embed = discord.Embed(
-            title=f"{username}'s Daily Claim",
-            description=f"You received a {settings.collectible_name}!",
-            color=discord.Color.from_rgb(0, 191, 255)
+            title=f"{username}'s Daily Pack!",
+            description=f"You received **{ball.country}**!",
+            color=color_choice
         )
         embed.add_field(
-            name=f"{emoji} {ball.country}",
+            name=f"{emoji} **{ball.country}** (Rarity: {ball.rarity})",
             value=f"``💖 {instance.attack_bonus}`` ``⚽ {instance.health_bonus}``"
         )
-        embed.set_footer(text="Come back in 24 hours for your next claim!")
 
-        await interaction.response.send_message(embed=embed)
+
+        # Generate card image
+        content, file, view = await instance.prepare_for_message(interaction)
+
+        # Attach the image to the embed
+        embed.set_image(url="attachment://" + file.filename)
+        embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+        embed.set_footer(text="Come back in 24 hours for your next claim! • Made by drift")
+
+
+        # Send the embed with the attached file
+        await interaction.response.send_message(embed=embed, file=file, view=view, ephemeral=False)
+
+        file.close()
+
         last_daily_times[user_id] = datetime.now()
